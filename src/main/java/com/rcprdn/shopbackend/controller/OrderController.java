1e081c9658f9981b7892f47d45a1b2e3643131b3
Rico Prodan <rico.prodan@gmail.com>
2023-09-13 11:43:15 +0200

    add get order

--- src/main/java/com/rcprdn/shopbackend/controller/OrderController.java
+++ src/main/java/com/rcprdn/shopbackend/controller/OrderController.java
@@ -16,46 +16,57 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class OrderController {
 
-  private final UserRepository userRepository;
-  private final CartRepository cartRepository;
-  private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
 
-  // TODO implement get order by userId and orderId
-//  @GetMapping("/{userId}/orders/{orderId}")
-//  public Order getOrderById(@PathVariable Long userId, @PathVariable Long orderId) {
-//    User user = userRepository.findById(userId)
-//            .orElseThrow();
-//
-//    Order order =
-//  }
+    // TODO implement get order by userId and orderId
+    @GetMapping("/{userId}/orders/{orderId}")
+    public Order getOrderById(@PathVariable Long userId, @PathVariable Long orderId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow();
 
-  @PostMapping("/{userId}/cart/submit")
-  public Order submitOrder(@PathVariable Long userId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow();
 
-    User user = userRepository.findById(userId)
-            .orElseThrow();
-
-    Cart cart;
-
-    Order order = null;
-
-    if (user.getCart() == null) {
-      System.out.println("Kein Warenkorb vorhanden");
-      // TODO add logic to handle empty order submission
-    } else {
-      cart = user.getCart();
-      order = new Order();
-      order.setUser(user);
-      order.setCart(cart);
-
-      order.setPaid(false);
-      order.setDate(new Date());
-      order.setShippingState(Order.ShippingState.PENDING);
-
-      orderRepository.save(order);
-      //user.getCart().clearCart();
-
+        return order;
     }
-    return order;
-  }
+
+    @GetMapping("/orders")
+    public Order getAllOrders() {
+
+        Order order = (Order) orderRepository.findAll();
+
+        return order;
+    }
+
+    @PostMapping("/{userId}/cart/submit")
+    public Order submitOrder(@PathVariable Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow();
+
+        Cart cart;
+
+        Order order = null;
+
+        if (user.getCart() == null) {
+            System.out.println("Kein Warenkorb vorhanden");
+            // TODO add logic to handle empty order submission
+        } else {
+            cart = user.getCart();
+            order = new Order();
+            order.setUser(user);
+            order.setCart(cart);
+
+            order.setPaid(false);
+            order.setDate(new Date());
+            order.setShippingState(Order.ShippingState.PENDING);
+
+            orderRepository.save(order);
+            //user.getCart().clearCart();
+
+        }
+        return order;
+    }
 }
